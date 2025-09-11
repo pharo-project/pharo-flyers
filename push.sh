291df0957d214e160d92d80e3faf7380421a37d0
StéphaneDucasse <stephane.ducasse@inria.fr>
2025-09-11 10:37:55 +0200

    adding the new incantationgiven by marcus

---
+++ push.sh
@@ -0,0 +1,3 @@
+ scp pharoCheatSheet.pdf  evref-pharo-files.lille.inria.fr:/var/www/html/pharo-files/media/
+
+#This needs the .ssh/config setup to tunnel all *.lille.inria.fr via ssh-ln if you do it from outside of the inria network.
